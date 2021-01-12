838199589dba78cf00b385622ad0222348e7ecee
EDaraya <65674097+EDaraya@users.noreply.github.com>
2021-01-12 11:06:25 -0600

    DataMemory Testbench

--- DataMemoryTb.v
+++ DataMemoryTb.v
@@ -1,10 +1,9 @@
 `timescale 10ns / 1ps
-// Data Memory Testbench
 /* 
 Objetivo: 
 	Comprobar el correcto almacenamiento y lectura de datos, dependiendo de sus entradas.
 	Debe probarse la correcta lectura de un dato para WE = 0 y la correcta escritura para WE = 1
-Procedimiento
+Procedimiento:
 	Escribir datos en distintas addres y comprobar que la salida posea el valor de estos.
 	En esta prueba debido a que WE es 1, el primer valor a esperar en RD es el valor de 15 en el byte más significativo. 
 	Este valor debe escribirse en el otro ciclo de reloj, por lo tanto
@@ -21,8 +20,8 @@ module DataMemoryTb;
 	reg [31:0] WD;
 	// Outputs
 	wire [31:0] RD;
-	// Instantiate the Device Under Test (UUT)
-	DataMemory DUT (
+	// Instantiate the Unit Under Test (uut)
+	DataMemory uut (
 		.clk(clk), 
 		.A(A), 
 		.WE(WE), 
@@ -31,28 +30,37 @@ module DataMemoryTb;
 		.RD(RD)
 	);
 	initial 
-	begin
-		// Initialize Inputs
-		#15
-		clk = 1;
-		A = 3;
-		WE = 1;
-		BE = 4'b1000;
-		WD = 15;
-
-		#15;		
-		WE = 0;
-		
-		#15;
-		A = 6;
-		WE = 1;
-		BE = 4'b0100;
-		WD = 255;
-		
-		#15;
-		WE=0;
-	end
+		begin
+			// Initialize Inputs
+			#15
+			clk = 1;
+			A = 0;
+			WE = 1;
+			BE = 4'b1000;
+			WD = 255;
+			#15;		
+			WE = 0;
+			#15;
+			A = 1;
+			WE = 1;
+			BE = 4'b0100;
+			WD = 255;
+			#15;
+			WE=0;
+			#15;
+			A = 2;
+			WE = 1;
+			BE = 4'b0010;
+			WD = 255;
+			#15;
+			WE=0;
+			#15;
+			A = 3;
+			WE = 1;
+			BE = 4'b0001;
+			WD = 255;
+		end
    always 
-	#5 clk =! clk;
+		#5 clk =! clk;
 endmodule
 
